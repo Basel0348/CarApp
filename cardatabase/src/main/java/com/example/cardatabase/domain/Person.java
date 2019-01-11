ba96a5776264c6b306c8d0937258f3c3ac1f558f
Basel Arkia <basel0348@gmail.com>
2019-01-11 09:58:04 +0200

    g

--- cardatabase/src/main/java/com/example/cardatabase/domain/Person.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/Person.java
@@ -11,9 +11,22 @@ public class Person {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	private String name;
+	public Person() {}
 	public Person(String name) {
 		super();
 		this.name=name;
 		
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 }
